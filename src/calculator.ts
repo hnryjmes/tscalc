8d20fea684dd45de56505b11a019be81a7af70d6
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-14 18:08:09 +0000

    Add method

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,3 +1,11 @@
 export class Calculator {
+  public value: number;
 
+  constructor() {
+    this.value = 0;
+  }
+
+  public add(n: number): number {
+    return n;
+  }
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -1,1 +1,14 @@
+import { expect } from "chai";
 import { Calculator } from "../src/calculator";
+
+describe("calculator", () => {
+  it("should initialise with a calculated value of 0", () => {
+    const calculator = new Calculator();
+    expect(calculator.value).to.equal(0);
+  });
+
+  it("should return a result of 5 when performing an addition of 5 immediately after creation", () => {
+    const calculator = new Calculator();
+    expect(calculator.add(5)).to.equal(5);
+  });
+});
